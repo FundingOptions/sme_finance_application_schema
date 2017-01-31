3600ce7efb30c0499b38648ebf988e1c3267335e
Nestor Arocha <nesaro@gmail.com>
2017-01-31 15:48:46 +0000

    improves translations

--- sme_finance_application_schema/translations.py
+++ sme_finance_application_schema/translations.py
@@ -1,11 +1,16 @@
 def sme_v5_and_contact_v3_to_finance_application_v3_translator(sme, sme_contact):
     applicant = {'first_name':sme_contact.get('applicant_first_name', ''),
-                 'surname': sme_contact.get('surname', '')
+                 'phone_number': sme_contact.get('phone_number'),
+                 'surname': sme_contact.get('applicant_surname', '')
                  }
     requesting_entity = {"name":sme_contact['sme_name']}
+    for field in ('legal_status', 'months_revenue', 'trade_credit', 'revenue', 'sic_code'):
+        if field in sme:
+            requesting_entity[field] = sme[field]
     finance_need = {}
-    if sme.get('legal_status'):
-        requesting_entity['legal_status'] = sme['legal_status']
+    for field in ('purpose','finance_term_length', 'date_finance_requested', 'requested_amount'):
+        if field in sme:
+            finance_need[field] = sme[field]
 
     return {
             'applicant': applicant,
